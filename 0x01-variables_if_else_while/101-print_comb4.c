d0c35516aec69a21131203774ffa41abafcf510b
PixepChief <yomama@example.com>
2023-06-16 13:20:14 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /**
  * main - prints all possible different combinations of three digits
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	int i, j, k;
+
 	for (i = 0; i < 10; i++)
 	{
 		for (j = i + 1; j < 10; j++)
