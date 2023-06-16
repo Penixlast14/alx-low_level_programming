722845fdab5858ad1cb5a2c2647452d3bc0adc92
PixepChief <yomama@example.com>
2023-06-16 00:57:54 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet without q and e.
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		if (i != 16 && i != 4)
+		{
+			putchar(alp[i]);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
